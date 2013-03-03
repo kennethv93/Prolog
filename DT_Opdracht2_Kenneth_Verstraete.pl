dcdbe267a27ec09e361b0abdfe5a6aa6d17300c4
kennethv93 <kennethverstraete@hotmail.com>
2013-03-03 02:17:41 +0100

    Invoegen opgave

---
+++ DT_Opdracht2_Kenneth_Verstraete.pl
@@ -0,0 +1,37 @@
+%Schrijf 3 predicaten die werken op bomen en lijsten - de representatie
+%van bomen volgt daarbij die gezien in de les van 21 februari.
+
+%1) maak_boom/2
+%
+%   arg1: gesloten lijst met getallen; input
+%   arg2: gebalanceerde boom die alle getallen uit arg1 bevat in prefix orde
+%
+%
+%voorbeeld:
+%
+%?- maak_boom([1,2,3,4,5],B).
+%
+%B = boom(boom(leeg,boom(leeg,leeg,3),2),boom(leeg,boom(leeg,leeg,5),4),1)
+
+
+
+
+%2) verzamel_waarden/2
+%
+%   arg1: gegeven boom
+%   arg2: output: lijst van waarden in de boom, in de infix orde
+%
+%verzamel_waarden(boom(boom(leeg,boom(leeg,leeg,3),2),boom(leeg,boom(leeg,leeg,5),4),1),Waarden).
+%
+%
+%?- verzamel_waarden(boom(boom(leeg,boom(leeg,leeg,3),2),boom(leeg,boom(leeg,leeg,5),4),1),Waarden).
+%
+%Waarden = [2,3,1,4,5]
+%
+%
+%Probeer dit met en zonder findall/3.
+
+
+
+
+%3) Schrijf een merge sort/2
